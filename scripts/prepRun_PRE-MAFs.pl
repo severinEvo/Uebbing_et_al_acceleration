e6c4c7cc1ca9b5c049bffac792cfe7853529b0ac
severinEvo <SUebbing@gmx.de>
2024-02-08 11:44:09 -0500

    Fixed typos and smaller errors in TFBS prep scripts

--- scripts/prepRun_PRE-MAFs.pl
+++ scripts/prepRun_PRE-MAFs.pl
@@ -7,24 +7,24 @@ my @chroms = ("chr1","chr2","chr3","chr4","chr5","chr6","chr7","chr8","chr9","ch
 	"chr11","chr12","chr13","chr14","chr15","chr16","chr17","chr18","chr19","chr20",
 	"chr21","chr22","chrX");
 
-open(OUT, ">PRE-MAFs.batch");
+open(OUT, ">PRE-MAFs-job.txt");
 foreach my $i (@chroms){
-	print OUT ". ~/.bashrc; mkdir -p data/MAFs/; cd data/MAFs/; ";
+	print OUT ". ~/.bashrc; mkdir -p data/MAFs/tmp_mafs/$i ; cd data/MAFs/; ";
 
 	print OUT "LIST=\$(perl -lane 'print \"\$F[0]:\$F[1]-\$F[2]\";' pCEs_$i\.bed); for j in \$LIST; ";
 	print OUT "do echo \"\$j\" | perl -lne '\$_ =~ s/(chr[XY0-9]+):([0-9]+)-([0-9]+)/\$1\\t\$2\\t\$3/; print;' >tmp_pCE_$i\.bed; ";
-	print OUT "maf_parse -g tmp_pCE_$i\.bed pCEs_$i\.maf >tmp_mafs/\"\$j\"\\.maf; ";
-	print OUT "done; ";
+	print OUT "maf_parse -g tmp_pCE_$i\.bed pCEs_$i\.maf >tmp_mafs/$i\/\"\$j\"\\.maf; ";
 	print OUT "rm tmp_pCE_$i\.bed; ";
+	print OUT "done; ";
 
 	print OUT "LIST=\$(perl -lane 'print \"\$F[0]:\$F[1]-\$F[2]\";' cCREs_$i\.bed); for j in \$LIST; ";
 	print OUT "do echo \"\$j\" perl -lne '\$_ =~ s/(chr[XY0-9]+):([0-9]+)-([0-9]+)/\$1\\t\$2\\t\$3/; print;' >tmp_cCRE_$i\.bed; ";
-	print OUT "maf_parse -g tmp_cCRE_$i\.bed cCREs_$i\.maf >tmp_mafs/\"\$j\"\\.maf; ";
-	print OUT "done; ";
-	print OUT "rm tmp_cCRE_$i\.bed\n";
+	print OUT "maf_parse -g tmp_cCRE_$i\.bed cCREs_$i\.maf >tmp_mafs/$i\/\"\$j\"\\.maf; ";
+	print OUT "rm tmp_cCRE_$i\.bed; ";
+	print OUT "done\n";
 }
 close(OUT);
 
 # Create jobs:
 # module load dSQ
-# dsq --job-file PRE-MAFs.batch --mem-per-cpu 6G --mail-type FAIL,END
+# dsq --job-file PRE-MAFs-job.txt --mem-per-cpu 6G --mail-type FAIL,END
--- scripts/prepRun_translate.pl
+++ scripts/prepRun_translate.pl
@@ -7,11 +7,11 @@ my @chroms = ("chr1","chr2","chr3","chr4","chr5","chr6","chr7","chr8","chr9","ch
 	"chr11","chr12","chr13","chr14","chr15","chr16","chr17","chr18","chr19","chr20",
 	"chr21","chr22","chrX");
 
-open(OUT, ">PRE-MAFs.batch");
+open(OUT, ">perl-translate-job.txt");
 foreach my $i (@chroms){
 	print OUT ". ~/.bashrc; mkdir -p data/MAFs/tmp_MAFs/$i ; cd data/MAFs/tmp_MAFs/$i ; ";
 	print OUT "module load BEDTools; ";
-	print OUT "for i in \$(ls chr1*.maf); do perl ~/whales/200120_120mammals/220920_meme/maf-translator.pl $i ; ";
+	print OUT "for i in \$(ls $i\*.maf); do perl ../../../../scripts/maf-translator.pl \$i ; ";
 	print OUT "done\n";
 }
 close(OUT);
